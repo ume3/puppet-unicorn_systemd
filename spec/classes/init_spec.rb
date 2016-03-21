aa73503b6923f51d4f89c649985faaa2e18dda67
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-03-21 16:48:10 +0900

    working_directroy must be set an absolute path (fix specs)

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 describe 'unicorn_systemd' do
 
+  let(:params) do
+    {
+      working_directory: '/srv',
+    }
+  end
+
   context 'with default values for all parameters' do
     it { should compile }
     it { should compile.with_all_deps }
